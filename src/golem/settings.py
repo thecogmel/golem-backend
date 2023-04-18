470843eee8126c7209e0dc10c10a2bea67d9ce3b
Erick Medeiros <erick.medeiros.104@ufrn.edu.br>
2023-04-18 20:50:48 -0300

    feat: :tada: begin golem backend project

--- src/golem/settings.py
+++ src/golem/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -111,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/Fortaleza"
 
 USE_I18N = True
 
