30fe7935bad46bbf52c1cc86f71797fd317bc713
Erick Medeiros <erick.medeiros.104@ufrn.edu.br>
2023-09-08 23:50:02 -0300

    build: :rocket: static files dir

--- src/golem/settings.py
+++ src/golem/settings.py
@@ -174,11 +174,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Diretório onde os arquivos estáticos serão coletados
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Diretórios de arquivos estáticos adicionais (opcional)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
